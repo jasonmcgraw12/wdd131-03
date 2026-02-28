059a0b880cf8dac066c5da48e0a9ead269bde15e
jasonmcgraw12 <jasonmcgraw12@byui.edu>
2026-02-27 23:14:08 -0700

    Testing favicons

--- character-card/script.js
+++ character-card/script.js
@@ -1,3 +1,32 @@
+const character = {
+    name: "Snortleblat",
+    class: "Swamp Beast Diplomat",
+    level: 5,
+    health: 100,
+    img: "snortleblat.webp"
+}
+
+const newCharacter = document.querySelector("div")
+newCharacter.innerHTML = `<div class="modal hidden">
+        ${character.name} died
+        <button id="okay">Okay</button>
+    </div>
+    <div id="character-card">
+        <img src=${character.img} alt="">
+        <div id="nameplate">
+            <h1>${character.name}</h1>
+        </div>
+        <div id="card-info">
+            <p><span class="info">Class: </span>${character.class}</p>
+            <p><span class="info">Level: </span><span id="level">${character.level}</span></p>
+            <p><span class="info">Health: </span><span id="health">${character.health}</span></p>
+        </div>
+        <div id="buttons">
+            <button id="attacked">Attacked</button>
+            <button id="level-up">Level Up</button>
+        </div>
+    </div>`;
+    
 const attackedBtn = document.querySelector("#attacked");
 const levelUpBtn = document.querySelector("#level-up");
 const levelLabel = document.querySelector("#level");
