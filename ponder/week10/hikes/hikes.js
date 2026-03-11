d9d152fd3be6f4c41f610e2024ef6de4ed9a32b5
jasonmcgraw12 <jasonmcgraw12@byui.edu>
2026-03-11 12:28:02 -0600

    Hike search website complete

--- ponder/week10/hikes/hikes.js
+++ ponder/week10/hikes/hikes.js
@@ -71,6 +71,95 @@ const hikes = [
   }
 ];
 
-let randomHike = hikes[Math.floor(Math.random()) * hikes.length];
+let hikeContainer = document.querySelector("#hike-container");
+let input = document.querySelector("#search");
+let button = document.querySelector("button");
 
-console.log(randomHike);
+button.addEventListener("click", search);
+
+
+
+function search() {
+  let hikeQuerry = input.value.toLowerCase();
+
+  let filteredHikes = hikes.filter(function(hike) {
+    return (
+      hike.name.toLowerCase().includes(hikeQuerry) ||
+      hike.description.toLowerCase().includes(hikeQuerry) ||
+      hike.tags.find(tag => tag.toLowerCase().includes(hikeQuerry))
+  );
+
+  
+  });
+  console.log(filteredHikes);
+
+  let sortedHikes = filteredHikes.sort(compareHikes);
+
+  function compareHikes(a,b) {
+    if (a.difficulty < b.difficulty) {
+        return -1;
+    } else if (a.difficulty > b.difficulty) {
+        return 1;
+    }
+    return 0;
+    }
+
+    hikeContainer.innerHTML = ``
+    sortedHikes.forEach(function(hike) {
+      
+      renderHike(hike);
+    });
+}
+
+
+
+let rndIndex = Math.floor(Math.random() * hikes.length);
+console.log(rndIndex);
+let rndHike = hikes[rndIndex];
+
+
+
+function tagTemplate(tags) {
+    return tags.map((tag)=> `<button>${tag}</button>`).join(' ');
+}
+
+function difficultyTemplate(rating) {
+		let html = `<span
+	class="rating"
+	role="img"
+	aria-label="Rating: ${rating} out of 5"
+>  Difficulty: `
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        html += `<span aria-hidden="true" class="icon-boot"> 🥾</span>`
+      } else {
+        html += `<span aria-hidden="true" class="icon-empty">▫️</span>`
+      }			
+    }
+    html += `</span>`
+    return html
+  }
+
+function hikesTemplate(hike) {
+    return `<div class="hike-card">
+  <div class="hike-content">
+    <h2>${hike.name}</h2>
+    <div class="hike-tags">
+      ${tagTemplate(hike.tags)}
+    </div>
+    <p>${hike.description}</p>
+    <p>${difficultyTemplate(hike.difficulty)}</p>
+  </div>
+</div>`
+}
+
+function renderHike(hike) {
+    let html = hikesTemplate(hike);
+    hikeContainer.innerHTML += html
+}
+
+function init() {
+    renderHike(hikes[rndIndex]);
+}
+
+init();
