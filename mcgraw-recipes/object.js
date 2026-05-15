0896a5e6960f4c2e1de904357dfa363acacb6fba
jasonmcgraw12 <jasonmcgraw12@byui.edu>
2026-05-15 09:50:06 -0600

    Fixed styling issues like headers being offset from what they were displaying, and checkbox text not being aligned with the checkbox

--- mcgraw-recipes/object.js
+++ mcgraw-recipes/object.js
@@ -34,7 +34,8 @@ const recipes = [
             "Add eggs and vanilla to creamed mixture.",
             "Combine flour and creamed mixtures together.",
             "Roll dough in cinnamon and sugar.",
-            "Put cookies in the oven for 9-11 minutes."
+            "Put cookies in the oven for 9-11 minutes.",
+            "Snicker your doodle."
         ]
     },
     {
@@ -90,8 +91,124 @@ const recipes = [
             "Put a pot on the stove and whisk together milk, sugar, and cocoa. (use a rubber whisk or a wooden spoon so you don't scratch your pan!).",
             "Let it cool and enjoy."
         ]
+    },
+    {
+        title: "Brownies",
+        description: "A yummy yummy brownie recipe",
+        difficulty: "☠️☠️⬛️⬛️⬛️",
+        time: "30min/30min",
+        yummyness: "5/5",
+        img: "images/square-brownies.jpg",
+        imgalt: "Image of delicious brownies that lived short lives",
+        sections: [
+            {
+                sectionName: "Chocolate Mix",
+                ingredients: [
+                    "Margarine: 3/4 cup", // this is a test. the old recipe called for 10 tablespoons of butter. 
+                    // I tried 1/2 cups instead and it turned out dry and grainy
+                    "Sugar: 1 1/4 cup",
+                    "Cocoa Powder: 3/4 cup",
+                    "Salt: 1/2 teaspoon"
+                ]
+            },
+            {
+                sectionName: "Wet Ingredients",
+                ingredients: [
+                    "Vanilla Extract: 1 teaspoon",
+                    "Eggs: 2 large"
+                ]
+            },
+            {
+                sectionName: "Dry Ingredients",
+                ingredients: [
+                    "Baking Powder: 1 teaspoon",
+                    "Flour: 1/2 cup"
+                ]
+            },
+            {
+                sectionName: "Optional Ingredients",
+                ingredients: [
+                    "Chocolate Chips: 1/2 cup",
+                    "Chopped Walnuts: 1/2 cup"
+                ]
+            }
+
+        ],
+        instructions: [
+            "Preheat oven to 325 degrees Fahrenheit.",
+            "Combine butter, sugar, cocoa powder, and salt in a large pan.",
+            "Cook the chocolate mix you just made on low heat (just hot enough to melt the butter) and stir occasionally",
+            "After mixture becomes a yummy chocolate paste, remove from heat and let the mixture cool for 5-10 minutes",
+            "While it's cooling whisk baking powder and flower together in a small bowl",
+            "When mixture is cool enough to not cook your eggs, stir in vanilla and eggs to the chocolate mix until combined",
+            "Add Dry ingredients to the chocolate mix until combined.",
+            "Fold in optional ingredients as desired.",
+            "Bake for 30 to 35 minutes (you can cook your eggs now)."
+        ]
     }
+   /*
+    , // change this comma
+    {
+        title: "Brownies",
+        description: "A yummy yummy brownie recipe",
+        difficulty: "☠️☠️⬛️⬛️⬛️",
+        time: "30min/30min",
+        yummyness: "5/5",
+        img: "images/hot-cocoa.jpg", // change this 
+        imgalt: "Image of pan of hot cocoa with a straw sticking out", // change this
+        sections: [
+            {
+                sectionName: "Chocolate Mix",
+                ingredients: [
+                    "Margarine: 1/2 cup",
+                    "Sugar: 1 1/4 cup",
+                    "Cocoa Powder: 3/4 cup"
+                    "Salt: 1/2 teaspoon"
+                ]
+            },
+            {
+                sectionName: "Dry Ingredients",
+                ingredients: [
+                    "Baking Powder: 1 teaspoon",
+                    "Flour: 1/2 cup"
+                ]
+            },
+            {
+                sectionName: "Wet Ingredients",
+                ingredients: [
+                    "Vanilla Extract: 1 teaspoon",
+                    "Eggs: 2 large"
+                ]
+            },
+            {
+                sectionName: "Optional Ingredients",
+                ingredients: [
+                    "Chocolate Chips: 1/2 cup",
+                    "Chopped Walnuts: 1/2 cup"
+                ]
+            }
+
+        ],
+        instructions: [
+            "Preheat oven to 325 degrees Fahrenheit.",
+            "Combine butter, sugar, cocoa powder, and salt in a large pan.",
+            "Cook the chocolate mix you just made on low heat (just hot enough to melt the butter) and stir occasionally",
+            "After mixture becomes a yummy chocolate paste, remove from heat and let the mixture cool for 5-10 minutes",
+            "While it's cooling whisk baking powder and flower together in a small bowl",
+            "When mixture is cool enough to not cook your eggs, stir in vanilla and eggs to the chocolate mix until combined",
+            "Add Dry ingredients to the chocolate mix until combined.",
+            "Fold in optional ingredients as desired.",
+            "Bake for 30 to 35 minutes (you can cook your eggs now)."
+        ]
+    }
+    */
 ];
+
+
+// Add Fudge, Brownies, oatmeal chocolate chip, oatmeal raisin, chocolate chip, slices or pielings(the dumpling pie recipe I want to make), biscuts, scones,
+// churros, prezzels, Madeleines, turnovers, dessert bars, Danish, ginger bread, lemon bread
+// With logo, could incorperate letters of my name
+// could make the whole logo in one line
 
 const commonIngredients = ["Flour",
                            "Eggs",
